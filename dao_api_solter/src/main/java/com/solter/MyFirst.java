fff1ad2168148625ffa74f988115a2d459281bf4
pedromsen@hotmail.com <pedromsen@hotmail.com>
2012-10-31 17:37:18 +0100

    hola

--- dao_api_solter/src/main/java/com/solter/MyFirst.java
+++ dao_api_solter/src/main/java/com/solter/MyFirst.java
@@ -1,13 +1,4 @@
 package com.solter;
 
-public class MyFirst {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public interface MyFirst {
 }
