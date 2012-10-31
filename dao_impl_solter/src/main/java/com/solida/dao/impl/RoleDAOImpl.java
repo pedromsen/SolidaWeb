90695f705776e3f5f4636d5f6b2bb8960148aaca
delabroza <delabroza@gmail.com>
2012-10-31 16:38:02 +0100

    Prueba de commit

--- dao_impl_solter/src/main/java/com/solida/dao/impl/RoleDAOImpl.java
+++ dao_impl_solter/src/main/java/com/solida/dao/impl/RoleDAOImpl.java
@@ -13,6 +13,7 @@ import com.solida.model.Role;
 public class RoleDAOImpl extends HibernateDaoSupport implements RoleDAO{
  
 	
+	/* Otro cambio */
 	public RoleDAOImpl() {
 		// TODO Auto-generated constructor stub Prueba
 	}
